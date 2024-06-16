9b70ace4b2572469502de94ac39985e1a85bcacc
Zyrakia <ole.lammers@pm.me>
2024-06-15 21:05:22 -0700

    init: setup commit

---
+++ src/main/java/dev/zyrakia/productiveplants/ProductivePlants.java
@@ -0,0 +1,9 @@
+package dev.zyrakia.productiveplants;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ProductivePlants implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/dev/zyrakia/productiveplants/client/ProductivePlantsClient.java
@@ -0,0 +1,9 @@
+package dev.zyrakia.productiveplants.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProductivePlantsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
